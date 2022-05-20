76e8ac0e22de391817fefaba999ca92b341acb1c
AyaSafan <ayasafan16@gmail.com>
2022-05-20 22:43:03 +0200

    add nav bar

--- zag-trader/src/pages/dashboard/dashboad.js
+++ zag-trader/src/pages/dashboard/dashboad.js
@@ -8,18 +8,33 @@ import PortfolioModule from './components/portfolioModule';
 import StratigesModule from './components/stratigesModule';
 import ContestsModule from './components/contestsModule';
 
+import {FaHome, FaChartBar, FaArtstation, FaBitcoin, FaAward}  from 'react-icons/fa';
+
+
 function Dashboard() {
     return (
     <body>
-        <div className ='dashboardGrid'>
-        <TotalFunds/>
-        <Banner/>
-        <WalletModule/>
-        <MarginModule/>  
-        <YieldModule/> 
-        <PortfolioModule/> 
-        <StratigesModule/>
-        <ContestsModule/>
+        <div>
+            <di className='mainmenu'>
+                <div className='mainmenue-logo'></div>
+                <ul>
+                    <li><a class="active" href="/"><FaHome size={24}/> &nbsp; Main</a></li>
+                    <li><a href="/"><FaChartBar size={24}/> &nbsp;Margin</a></li>
+                    <li><a href="/"><FaBitcoin size={24}/> &nbsp;Buy crypto</a></li>
+                    <li><a href="/"><FaArtstation size={24}/> &nbsp;Yield</a></li>
+                    <li><a href="/"><FaAward size={24}/> &nbsp; Contests</a></li>
+                </ul>
+            </di>
+            <div className ='dashboardGrid'>
+            <TotalFunds/>
+            <Banner/>
+            <WalletModule/>
+            <MarginModule/>  
+            <YieldModule/> 
+            <PortfolioModule/> 
+            <StratigesModule/>
+            <ContestsModule/>
+            </div>
         </div>
     </body>
     );
